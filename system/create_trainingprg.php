fda5c1196551fac5bcbb861811d79a74003c0431
Zed.Zefi <zed.zefi@gmail.com>
2018-08-02 16:17:48 +0800

    Update Create training

---
+++ system/create_trainingprg.php
@@ -0,0 +1,34 @@
+<?php
+  session_start();
+  if (isset($_SESSION["Trainer_id"])) {
+    // code...
+    $title = $_POST["title"];
+    $the_date = $_POST["date"];
+    $the_time = $_POST["time"];
+    $fee = $_POST["fee"];
+    $status = $_POST["status"];
+    $trainer_id = $_SESSION["Trainer_id"];
+    $max_participant = $_POST["max_participant"];
+
+  	include '../conf/conn.php';
+
+  	$sql = "INSERT INTO training_session (title, the_date, the_time, fee, status, trainer_id, max_participant)
+  		VALUES ('$title', '$the_date', '$the_time', '$fee', '$status', '$trainer_id', 
+      '$max_participant')";
+
+		if ($con->query($sql) === TRUE) {
+			echo "New record created successfully";
+		    header("Location: ../public/dash.php"); /* Redirect browser */
+			exit();
+		} else {
+			echo "Error: " . $sql . "<br>" . $con->error;
+		    header("Location: ../public/create_training.php"); /* Redirect browser */
+			exit();
+		}
+  	$con->close();
+  } else {
+    // code...
+    header("Location; ../public/dash.php");
+  }
+
+?>
